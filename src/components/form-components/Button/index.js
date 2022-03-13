2e02f5544639792e9aa39ed129e3c49a1c848f4a
KostaLaz <kosta.lazarevski89@gmail.com>
2022-03-13 19:35:52 +0100

    Button component added

---
+++ src/components/form-components/Button/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import { useFormikContext } from "formik";
+
+const ButtonWrapper = () => {
+
+    return (
+        <Button />
+    );
+
+    export default ButtonWrapper;
+}
