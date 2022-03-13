e18984272101a4c35c77bcd1c9ad966a382581b6
KostaLaz <kosta.lazarevski89@gmail.com>
2022-03-13 18:50:14 +0100

    Login form changes

--- src/pages/login-page/components/login-form/LoginForm.js
+++ src/pages/login-page/components/login-form/LoginForm.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import { useFormik } from 'formik';
-import * as Yup from 'yup'; 
+import { Form, useFormik } from 'formik';
+import * as Yup from 'yup';
+import Grid from '@material-ui/core/Grid';
+import { Container, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import { Formik } from 'formik';
 
 function LoginForm() {
+
+  const useStyles = makeStyles((theme) => ({
+    formWrapper: {
+      marginTop: theme.spacing(5),
+      marginBottom: theme.spacing(8),
+    },
+  }))
 
    const formik = useFormik({
        initialValues: {
@@ -20,111 +31,51 @@ function LoginForm() {
        }
    });
 
+   const INITIAL_VALUES = {
+    email: "",
+    username: "",
+    password: ""
+}
+
+  const FORM_VALIDATION = Yup.object().shape({
+
+  })
+
    console.log('ERR:: ', formik.errors);
 
   return (
-    <>
-        <form onSubmit={formik.handleSubmit}>
-            <div className="input-container">
-                <label>Email: </label>
-                <input 
-                id="email"
-                type="text"
-                name="email"
-                placeholder="Enter email"
-                onChange={formik.handleChange}
-                value={formik.values.email} />
-                
-            </div>
-            <div className="input-container">
-                <label>Username: </label>
-                <input 
-                id="username"
-                type="text"
-                name="username"
-                placeholder="Username"
-                onChange={formik.handleChange}
-                value={formik.values.username} />
-            </div>
-            <div className="input-container">
-                <label>Password: </label>
-                <input 
-                id="password"
-                type="text"
-                name="password"
-                placeholder="password"
-                onChange={formik.handleChange}
-                value={formik.values.password} />
-            </div>
-            <button type="submit">Submit</button>
-        </form>
-        {/* <h1>Your Data</h1>
-    <Formik
-      initialValues={{ userName: "", email: "", password: "", acceptedTerms: false }}
-      validate={(values) => {
-        const errors = {};
-        if (!values.name) {
-          errors.name = "Required";
-        }
-
-        if (!values.acceptedTerms) {
-          errors.acceptedTerms =
-            "You must accept the terms and conditions before you proceed.";
-        }
-
-        if (!values.email) {
-          errors.email = "Required";
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = "Invalid email address";
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        // post data to server
-        alert(JSON.stringify(values, null, 2));
-        setSubmitting(false);
-      }}
-    >
-      {({ isSubmitting, dirty, handleReset }) => (
-        <Form>
-          <div>
-            <label>
-              Username
-              <Field type="text" name="name" />
-            </label>
-            <ErrorMessage name="name" component="span" />
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <Field type="email" name="email" />
-            <ErrorMessage name="email" component="span" />
-          </div>
-          <div>
-              <label>
-
-              </label>
-          </div>
-          <div>
-            <label>Accept terms</label>
-            <Field type="checkbox" name="acceptedTerms" />
-            <ErrorMessage name="acceptedTerms" component="span" />
-          </div>
-          <button
-            type="button"
-            onClick={handleReset}
-            disabled={!dirty || isSubmitting}
-          >
-            Reset
-          </button>
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </Form>
-      )}
-    </Formik> */}
-  </>
+    <Grid container>
+    <Grid item xs={12}>
+      <Container maxWidth="md">
+        <Formik
+          initialValues={{
+            ...INITIAL_VALUES
+          }}
+          validationSchema={FORM_VALIDATION}
+          onSubmit={values => {
+            console.log('VALUES:: ', values);
+          }}
+            >
+          
+          <Form>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Typography>
+                  Email
+                </Typography>
+                <Typography>
+                  Username
+                </Typography>
+                <Typography>
+                  Password
+                </Typography>
+              </Grid>
+            </Grid>
+          </Form>
+        </Formik>
+      </Container>
+    </Grid>
+   </Grid>
 );
 }
 
