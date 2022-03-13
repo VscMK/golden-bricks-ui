677753e09941861b59df7dabb0973c126001edb7
KostaLaz <kosta.lazarevski89@gmail.com>
2022-03-13 19:41:02 +0100

    Button config

--- src/components/form-components/Button/index.js
+++ src/components/form-components/Button/index.js
@@ -2,12 +2,25 @@ import React from "react";
 import { Button } from "@material-ui/core";
 import { useFormikContext } from "formik";
 
-const ButtonWrapper = () => {
+const ButtonWrapper = ({children, otherProps}) => {
 
-    
+    const { submitForm } = useFormikContext();
+
+    const handleSubmit = () => {
+        submitForm();
+    };
+
+    const configButton = {
+        variant: 'contailned',
+        color: 'primary',
+        fullWidth: true,
+        onclick: handleSubmit,
+    }
 
     return (
-        <Button />
+        <Button
+            {...configButton}
+        />
     );
 
     export default ButtonWrapper;
