e53cd4f865f127b2e9ffbd12e831cbeab7011b9f
KostaLaz <kosta.lazarevski89@gmail.com>
2022-03-17 20:13:58 +0100

    store added

--- src/index.js
+++ src/index.js
@@ -4,13 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { configureStore } from '@reduxjs/toolkit';
+import store from './redux/store';
 import { Provider } from 'react-redux';
 
-const store = configureStore({
-  reducer: {
-    // users:
-  }
-});
 
 ReactDOM.render(
   <React.StrictMode>
