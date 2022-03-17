1648ca1762a762ca46d33412d2e623062fa48e88
KostaLaz <kosta.lazarevski89@gmail.com>
2022-03-17 19:47:32 +0100

    userService

---
+++ src/services/user-service/userService.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+
+const API_URL = "http://localhost:8080/api/test/";
+
+const getUserBoard = () => {
+  return axios.get(API_URL + "user", { headers: authHeader() });
+};
+
+const getAdminObserverBoard = () => {
+  return axios.get(API_URL + "mod", { headers: authHeader() });
+};
+
+const getAdminBoard = () => {
+  return axios.get(API_URL + "admin", { headers: authHeader() });
+};
+
+const getBeekeeperBoard = () => {
+    return axios.get(API_URL + 'beekeeper', { headers: authHeader() });
+}
+const getBeekeepersCoordinatorBoard = () => {
+    return axios.get(API_URL + 'beekeepers-coordinator', {headers: authHeader()});
+}
+
+const userService = {
+  getBeekeeperBoard,
+  getUserBoard,
+  getAdminObserverBoard,
+  getAdminBoard,
+};
+export default userService;
