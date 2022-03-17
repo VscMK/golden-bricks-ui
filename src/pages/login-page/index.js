b6342188b282535f1845ebe40819f17b541aeb46
KostaLaz <kosta.lazarevski89@gmail.com>
2022-03-17 20:22:12 +0100

    comments removed

--- src/pages/login-page/index.js
+++ src/pages/login-page/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { useState } from 'react';
 import LoginForm from './components/login-form/LoginForm';
+import { Grid } from '@material-ui/core';
 
 const LoginPage = () => {
+    // const styles = {
+    //     centerLoginForm: {
+    //         display: "flex",
+    //         justify-content: "center",
+            
+    //     }
+    // }
 
     const login = (details) => {
         console.log(details)
     }
 
   return (
-    <div>
+    <Grid container direction="row" alignItems="center" justify="center">
+        <div className="center-form">
             <LoginForm />
-    </div>
+        </div>
+    </Grid>
   )
 }
 
