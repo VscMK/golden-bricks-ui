ec056eb71df977648fed0f23d19eaf74b2bf7b85
KostaLaz <kosta.lazarevski89@gmail.com>
2022-03-17 17:14:59 +0100

    commit

--- src/pages/login-page/components/login-form/LoginForm.js
+++ src/pages/login-page/components/login-form/LoginForm.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Form, Formik } from 'formik';
+import { Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
 import Grid from '@material-ui/core/Grid';
 import { Container, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core';
 import TextFieldWrapper from '../../../../components/form-components/TextField';
-import Button from '../../../../components/form-components/Button';
 import ButtonWrapper from '../../../../components/form-components/Button';
 
 function LoginForm() {
 
-  const useStyles = makeStyles((theme) => ({
-    formWrapper: {
-      marginTop: theme.spacing(5),
-      marginBottom: theme.spacing(8),
+  const useStyles = makeStyles(() => ({
+    // formWrapper: {
+    //   marginTop: theme.spacing(5),
+    //   marginBottom: theme.spacing(8),
+    // },
+    btn: {
+      fontSize: '50px',
     },
   }))
 
@@ -25,56 +27,57 @@ function LoginForm() {
     password: ""
 }
 
-  const FORM_VALIDATION = Yup.object().shape({
-    email: Yup.string().email('Invalid email address.').required('Please enter your email.'),
-    username: Yup.string().required('Please enter your username.'),
-    password: Yup.string().required('Please enter your password.'),
-  })
-
   return (
     <Grid container>
     <Grid item xs={12}>
       <Container maxWidth="md">
-        {/* <div className={classes.formWrapper}> */}
+        <div className={classes.formWrapper}>
         <Formik
           initialValues={{
             ...INITIAL_VALUES
           }}
-          validationSchema={FORM_VALIDATION}
+          validationSchema={Yup.object().shape({
+            email: Yup.string()
+              .email("Email is invalidd")
+              .required("Email is required"),
+            username: Yup.string().required("Username is required"),
+            password: Yup.string().required("Password is required")
+          })}
           onSubmit={values => {
             console.log('VALUES:: ', values);
           }}
             >
-          
           <Form>
             <Grid container spacing={1}>
               <Grid item xs={12}>
                 <Typography >
-                  Enter your credentials:
+                  Enter your credentials to login:
                 </Typography>
                 <TextFieldWrapper 
                 name='email'
-                label={'Email:'} />
+                label={'Email:'} 
+                />
               </Grid>
               <Grid item xs={12}>
                 <TextFieldWrapper 
                 name='username'
-                label={'Username:'} />
+                label={'Username:'}                />
               </Grid>
               <Grid item xs={12}>
                 <TextFieldWrapper 
                 name='password'
-                label={'Password:'} />
+                label={'Password:'}
+                />
               </Grid>
               <Grid item xs={12}>
-                <ButtonWrapper>
-                  Submit form
+                <ButtonWrapper className={classes.btn}>
+                  Login
                 </ButtonWrapper>
               </Grid>
             </Grid>
           </Form>
         </Formik>
-        {/* </div> */}
+        </div>
       </Container>
     </Grid>
    </Grid>
--- src/pages/login-page/index.js
+++ src/pages/login-page/index.js
@@ -3,13 +3,6 @@ import { useState } from 'react';
 import LoginForm from './components/login-form/LoginForm';
 
 const LoginPage = () => {
-    // const adminUser = {
-    //     email: "admin@email.com",
-    //     password: "password123"
-    // }
-
-    // const [user, setUser] = useState({name: "", email: "" });
-    // const [error, setError] = useState("");
 
     const login = (details) => {
         console.log(details)
@@ -17,15 +10,7 @@ const LoginPage = () => {
 
   return (
     <div>
-        {/* {(user.email != "") ? ( */}
-            {/* <div className="welcome">
-                <h2>Welcome, <span>user.name</span></h2>
-            </div> */}
             <LoginForm />
-        {/* )  */}
-        {/* // : (
-        //     <LoginForm />
-        // )} */}
     </div>
   )
 }
