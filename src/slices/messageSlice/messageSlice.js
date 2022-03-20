32af0a22cf8802f2ffdf30672fafe2b6d025b478
KostaLaz <kosta.lazarevski89@gmail.com>
2022-03-20 15:39:00 +0100

    typo fix

--- src/slices/messageSlice/messageSlice.js
+++ src/slices/messageSlice/messageSlice.js
@@ -17,6 +17,6 @@ const messageSlice = createSlice({
 
 const { reducer, actions } = messageSlice;
 
-export const { setMessage, clearMessage } = actions
+export const { setMessage, clearMessage } = actions;
 
 export default reducer;
