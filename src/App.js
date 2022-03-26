0f66b8c79c50060ec66a7f00455e39bcc5c26702
KostaLaz <kosta.lazarevski89@gmail.com>
2022-03-26 19:03:04 +0100

    Routing for login and register added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import LoginPage from './pages/login-page';
+import RegisterPage from './pages/register-page';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<LoginPage/>} />
+        <Route path='/register' element={<RegisterPage/>} />
+        {/* <div className="App">
+        < LoginPage />
+        </div> */}
+      </Routes>
+    </Router>
   );
 }
 
