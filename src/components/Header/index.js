a66b06a4a1c6f2ffd1d0bb03cf56dc08426d9d2b
KostaLaz <kosta.lazarevski89@gmail.com>
2022-04-04 18:32:04 +0200

    Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { AppBar, Typography, Toolbar, Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+
+const Header = () => {
+    
+    console.log('HEADER');
+
+    const useStyles = makeStyles({
+        toolbar: {
+          backgroundColor: '#f8b133',
+          color: 'rgba(0,0,0,.87)',
+        },
+      });
+    const classes = useStyles();
+    
+    
+    return (
+       <React.Fragment>
+           <AppBar className={classes.toolbar}>
+               <Toolbar>
+                   <Typography>
+                   <Box sx={{ fontWeight: 'bold', m: 1, letterSpacing: 4}}>
+                       GOLDEN BRICKS
+                   </Box>
+                   </Typography>
+               </Toolbar>
+           </AppBar>
+       </React.Fragment>
+    );
+};
+
+export default Header;
