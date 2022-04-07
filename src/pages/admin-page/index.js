df3331caed702981ece72128b4eb956ef3d535e6
KostaLaz <kosta.lazarevski89@gmail.com>
2022-04-07 20:14:20 +0200

    admin page added

---
+++ src/pages/admin-page/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/Header';
+import LayoutWrapper from '../../components/layout';
+
+function AdminPage() {
+  return (
+   <LayoutWrapper>
+       <div>ADMIN</div>
+   </LayoutWrapper>
+  )
+}
+
+export default AdminPage;
