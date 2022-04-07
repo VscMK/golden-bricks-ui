371437fdeb1d914c21a1509ac4929b42041dd83b
KostaLaz <kosta.lazarevski89@gmail.com>
2022-04-07 23:52:52 +0200

    airpays route added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LoginPage from './pages/login-page';
 import RegisterPage from './pages/register-page';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminPage from './pages/admin-page';
+import AirpaysPage from './pages/airpays-page';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<LoginPage/>} />
         <Route path='/register' element={<RegisterPage/>} />
         <Route path='/admin-page' element={<AdminPage />} />
+        <Route path='/airpays-page' element={<AirpaysPage />} />
         {/* <div className="App">
         < LoginPage />
         </div> */}
