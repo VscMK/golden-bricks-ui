bfb08877db09530ac82cb7cf58137165a23e9940
KostaLaz <kosta.lazarevski89@gmail.com>
2022-04-07 20:13:33 +0200

    routing for admin page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import LoginPage from './pages/login-page';
 import RegisterPage from './pages/register-page';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AdminPage from './pages/admin-page';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<LoginPage/>} />
         <Route path='/register' element={<RegisterPage/>} />
+        <Route path='/admin-page' element={<AdminPage />} />
         {/* <div className="App">
         < LoginPage />
         </div> */}
