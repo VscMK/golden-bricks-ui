50b492ffdea276a33b1c7b514341f55bfe9c07dd
KostaLaz <kosta.lazarevski89@gmail.com>
2022-04-12 00:53:07 +0200

    Header and basic user info added

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,36 +1,47 @@
-import React from "react";
-import { Link, Redirect } from 'react-router-dom';
+import {React, useEffect, useState} from "react";
+import { Link } from 'react-router-dom';
 import { useSelector } from "react-redux";
+import Header from '../Header'
+import { Container, Grid, makeStyles } from '@material-ui/core';
+import { Typography } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
 
 const Profile = () => {
+
   const { user: currentUser } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
   if (!currentUser) {
-    return <Link to="/login" />;
+    return navigate('/login');
   }
+
   console.log('PROFILE ::::: ', currentUser);
   return (
-    <div className="container">
-      <header className="jumbotron">
-        <h3>
-          <strong>{currentUser.username}</strong> Profile
-        </h3>
-      </header>
-      <p>
-        <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{" "}
-        {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-      </p>
-      <p>
-        <strong>Id:</strong> {currentUser.id}
-      </p>
-      <p>
-        <strong>Email:</strong> {currentUser.email}
-      </p>
-      <strong>Authorities:</strong>
-      <ul>
-        {currentUser.roles &&
-          currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
-      </ul>
-    </div>
+    <Container >
+        <Header />
+        <Grid style={{ marginTop: '100px' }}>
+          <Typography variant='h3' >
+            Profile {currentUser.username}
+            </Typography>
+          <Typography variant='h3' >
+            Token:{currentUser.accessToken.substring(0, 20)} ...{" "}
+            {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
+          </Typography>
+          <Typography variant='h3' >
+            Id:{currentUser.id}
+          </Typography>
+          <Typography variant='h3' >
+            Email:{currentUser.email}
+          </Typography>
+          <Typography variant='h3'>
+            Authorities:
+          </Typography>
+        {/* <ul>
+          {currentUser.roles &&
+            currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
+        </ul> */}
+        </Grid>
+      </Container >
   );
 };
 export default Profile;
