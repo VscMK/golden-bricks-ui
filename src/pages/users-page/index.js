7ab5f34bb65983359504506766ab3b934bc457a6
KostaLaz <kosta.lazarevski89@gmail.com>
2022-04-11 22:23:25 +0200

    Users page added

---
+++ src/pages/users-page/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import LayoutWrapper from '../../components/layout'
+
+const UsersPage = () => {
+
+    
+
+  return (
+    <LayoutWrapper>
+        <div>UsersPage</div>
+    </LayoutWrapper>
+  )
+}
+
+export default UsersPage;
