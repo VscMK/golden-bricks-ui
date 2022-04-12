0afd9ae0aa205dd858536555c1827fd58baaf4fb
KostaLaz <kosta.lazarevski89@gmail.com>
2022-04-12 17:03:05 +0200

    Create airpay form added

---
+++ src/pages/airpays-page/create-airpay-form/index.js
@@ -0,0 +1,120 @@
+import { React, useEffect, useState } from 'react';
+import { Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import Grid from '@material-ui/core/Grid';
+import { Container, Typography } from '@material-ui/core';
+import TextfieldWrapper from '../../../components/form-components/TextField';
+import ButtonWrapper from '../../../components/form-components/Button';
+import { useDispatch, useSelector } from 'react-redux';
+// import { login } from '../../../../slices/authSlice/authSlice';
+// import { clearMessage } from '../../../../slices/messageSlice/messageSlice';
+import { useNavigate } from "react-router-dom";
+import Header from '../../../components/Header';
+
+const CreateAirpayForm = (props) => {
+
+  const [loading, setLoading] = useState(false);
+//   const { isLoggedIn } = useSelector((state => state.auth));
+//   const { message } = useSelector((state => state.message));
+  const dispatch = useDispatch();
+  let navigate = useNavigate();
+
+    const INITIAL_VALUES = {
+        name: '',
+        locationName: '',
+        noColonies: '',
+        fence: '',
+        electricity: '',
+    }
+    
+    const VALIDATION_SCHEMA = Yup.object().shape({
+        name: Yup.string().required("Firstname is required"),
+        locationName: Yup.string().required("Lasttname is required"),
+        noColonies: Yup.string().required("Number of colonies is required"),
+        fence: Yup.string().required('REQUIRED'),
+        electricity: Yup.string().required('ELECTRICITYYY')
+      });
+    
+    const handleSubmit = (formValue) => {
+    //   const { email, password } = formValue;
+    //   setLoading(true);
+    //   dispatch(login({ email, password }))
+    //     .unwrap()
+    //     .then(() => {
+    //       props.history.push("/profile");
+    //       window.location.reload();
+    //     })
+    //     .catch(() => {
+    //       setLoading(false);
+    //     });
+    // const {name, location, numberOfColonies, fence, electricity} = formValue;
+    console.log('FORM VALUE', formValue);
+    };
+
+
+  return (
+        <Grid container>
+            <Header />
+        <Grid item xs={12} style={{ marginTop: '70px' }}>
+          <Container maxWidth="md">
+            <Formik
+              initialValues={{
+                ...INITIAL_VALUES
+              }}
+              validationSchema={VALIDATION_SCHEMA}
+              onSubmit={handleSubmit}
+                >
+              <Form>
+                <Grid container spacing={1}>
+                  <Grid item xs={12}>
+                    <Typography variant='h4' component='h2' gutterBottom >
+                      Create new airpay: 
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextfieldWrapper 
+                    name='name'
+                    label={'Name:'} 
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextfieldWrapper 
+                    name='locationName'
+                    label={'Location:'} 
+                    />
+                   </Grid>
+                   <Grid item xs={12}>
+                    <TextfieldWrapper 
+                    name='noColonies'
+                    label={'Number of colonies:'} 
+                    />
+                  </Grid>
+                 <Grid item xs={12}>
+                    <TextfieldWrapper 
+                    name='fence'
+                    label={'Fence:'}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextfieldWrapper 
+                    name='electricity'
+                    label={'Electricity:'}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <ButtonWrapper disabled={loading} >
+                    {loading ? (
+                      <span className="spinner-border spinner-border-sm"></span>
+                    ) : 'Submit'}
+                    </ButtonWrapper>
+                  </Grid>
+                </Grid>
+              </Form>
+            </Formik>
+          </Container>
+        </Grid>
+       </Grid>
+    );
+}
+
+export default CreateAirpayForm;
