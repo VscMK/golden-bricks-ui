50e4ba6e8657bd530bd2dbb99c96f75bdef1223c
KostaLaz <kosta.lazarevski89@gmail.com>
2022-04-12 18:36:44 +0200

    get all airpays

--- src/pages/airpays-page/index.js
+++ src/pages/airpays-page/index.js
@@ -1,11 +1,35 @@
-import { Container } from '@material-ui/core';
-import React from 'react'
+import {React, useEffect, useState} from 'react';
+import { Container, Grid } from '@material-ui/core';
 import CreateAirpayForm from './create-airpay-form';
+import Header from '../../components/Header';
+import AirpayService from '../../services/airpay-service/airpayService';
 
 function AirpaysPage() {
+
+  const [airpays, setAirpays] = useState([]);
+  useEffect(() => {
+    AirpayService.getAirpays().then(
+      (response) => {
+        setAirpays(response.data);
+      },
+      (error) => {
+        const content =
+          (error.response && error.response.data) ||
+          error.message ||
+          error.toString();
+        setAirpays(content);
+      }
+    );
+  }, []);
+
+  console.log('AIRPAYS :: ', airpays);
+
   return (
     <Container>
-        <CreateAirpayForm />
+         <Header />
+         <Grid style={{ marginTop: '70px' }}>
+            <CreateAirpayForm />
+        </Grid>
     </Container>
   )
 }
