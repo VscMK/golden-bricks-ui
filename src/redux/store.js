4e07e1f104a50ef97fe9169b023b4c8af4538748
KostaLaz <kosta.lazarevski89@gmail.com>
2022-04-12 18:08:08 +0200

    airpayReducer imported

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from '../slices/authSlice/authSlice';
 import messageReducer from '../slices/messageSlice/messageSlice';
+import airpayReducer from '../slices/airpaySlice/airpaySlice';
 
 export default configureStore({
     reducer: {
         auth: authReducer,
-        message: messageReducer
+        message: messageReducer,
+        airpays: airpayReducer,
     },
 });
