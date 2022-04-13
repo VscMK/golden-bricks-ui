8013da9a461bfdae9f719eb2eda821523f5b5bdc
KostaLaz <kosta.lazarevski89@gmail.com>
2022-04-14 00:23:42 +0200

    get apiarys

--- src/pages/airpays-page/index.js
+++ src/pages/airpays-page/index.js
@@ -3,25 +3,24 @@ import { Container, Grid } from '@material-ui/core';
 import CreateAirpayForm from './create-airpay-form';
 import Header from '../../components/Header';
 import AirpayService from '../../services/airpay-service/airpayService';
+import { getAirpays } from '../../slices/airpaySlice/airpaySlice';
+import { useSelector,useDispatch } from 'react-redux';
 
 function AirpaysPage() {
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const { airpays } = useSelector((state => state.airpays));
 
-  const [airpays, setAirpays] = useState([]);
-  
-  useEffect(() => {
-    AirpayService.getAirpays().then(
-      (response) => {
-        setAirpays(response.data);
-      },
-      (error) => {
-        const content =
-          (error.response && error.response.data) ||
-          error.message ||
-          error.toString();
-        setAirpays(content);
-      }
-    );
-  }, []);
+  useEffect (() => {
+    setLoading(true);
+  dispatch(getAirpays())
+    .unwrap()
+    .then(() => {
+    })
+    .catch(() => {
+      setLoading(false);
+    });
+  },[]);
 
   console.log('AIRPAYS :: ', airpays);
 
