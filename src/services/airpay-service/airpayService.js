de3a5838d24c818c1d88de954523b4b90637154f
KostaLaz <kosta.lazarevski89@gmail.com>
2022-04-14 00:20:49 +0200

    Add Bearer token

--- src/services/airpay-service/airpayService.js
+++ src/services/airpay-service/airpayService.js
@@ -3,25 +3,30 @@ import authHeader from '../auth-header/authHeader';
 
 const API_URL = 'http://localhost:8081/';
 
-const getAirpays = (name, locationName, noColonies, fence, electricity) => {
+axios.interceptors.request.use(
+  config => {
+    config.headers.authorization = authHeader();
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  } 
+)
+
+const getAirpays = () => {
+  console.log('VLEZE VO API CALL &&&&&&&&&&&');
     return axios
-      .get(API_URL + 'apiary', { headers: authHeader() },
-      {
-        name,
-        locationName,
-        noColonies,
-        fence,
-        electricity
-    })
+      .get(API_URL + 'apiary')
       .then((response) => {
+        console.log('RES :::::::::::: ', response.data);
         return response.data;
       });
   };
 
   const addAirpay = (name, locationName, noColonies, fence, electricity) => {
-    console.log('VLEZE VO API CALL ZA ADD :: ', name, locationName, noColonies, fence, electricity);
+    // console.log('VLEZE VO API CALL ZA ADD :: ', name, locationName, noColonies, fence, electricity);
     return axios
-      .post(API_URL + 'apiary/create', { headers: authHeader() },
+      .post(API_URL + 'apiary/create',
       {
         name,
         locationName,
