c2db9153d500a819093a05aa52757b13448b4fe9
KostaLaz <kosta.lazarevski89@gmail.com>
2022-04-25 18:05:13 +0200

    Apiarys table component added

--- src/pages/airpays-page/index.js
+++ src/pages/airpays-page/index.js
@@ -1,39 +1,19 @@
-import {React, useEffect, useState} from 'react';
+import {React} from 'react';
 import {
   Container,
   Grid,
-  Typography, 
   Button, 
-  TableContainer, 
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  Paper,
   makeStyles
  } from '@material-ui/core';
 import Header from '../../components/Header';
-import { getAirpays } from '../../slices/airpaySlice/airpaySlice';
-import { useSelector,useDispatch } from 'react-redux';
+// import { getAirpays } from '../../slices/airpaySlice/airpaySlice';
+
 import { useNavigate } from 'react-router-dom';
+import ApiarysTable from './apiarys-table';
 
 function AirpaysPage() {
-  const [loading, setLoading] = useState(false);
-  const dispatch = useDispatch();
+  
   const navigate = useNavigate();
-  const { airpays } = useSelector((state => state.airpays));
-
-  useEffect (() => {
-    setLoading(true);
-  dispatch(getAirpays())
-    .unwrap()
-    .then(() => {
-    })
-    .catch(() => {
-      setLoading(false);
-    });
-  },[]);
 
   const useStyles = makeStyles({
     btn: {
@@ -51,7 +31,7 @@ function AirpaysPage() {
   return (
     <Container>
          <Header />
-         <Grid style={{ marginTop: '70px' }}>
+         <Grid style={{ marginTop: '100px' }}>
           <Button  
           className={classes.btn}
           onClick={() => {
@@ -59,37 +39,10 @@ function AirpaysPage() {
           }} >
             Add new apiary
           </Button>
-        </Grid> 
-         <Grid style={{ marginTop: '100px' }}>
-         < TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="caption table">
-        {/* <caption>A basic table example with a caption</caption> */}
-        <TableHead>
-          <TableRow>
-            <TableCell align="right">Name</TableCell>
-            <TableCell align="right">Location&nbsp;</TableCell>
-            <TableCell align="right">Number of colonies&nbsp;</TableCell>
-            <TableCell align="right">Fence&nbsp;</TableCell>
-            <TableCell align="right">Electricity&nbsp;</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {airpays && airpays.map((row) => (
-            <TableRow key={row.name}>
-              {/* <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell> */}
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.location_name}</TableCell>
-              <TableCell align="right">{row.no_colonies}</TableCell>
-              <TableCell align="right">{row.fence}</TableCell>
-              <TableCell align="right">{row.electricity}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    </Grid>
+        </Grid>
+        <Grid>
+          <ApiarysTable />
+        </Grid>  
     </Container>
   )
 }
