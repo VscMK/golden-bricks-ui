621db97e703d95988d6c1f12198b9635c180939e
KostaLaz <kosta.lazarevski89@gmail.com>
2022-05-08 19:35:58 +0200

    update apiary page

---
+++ src/pages/update-apiary-page/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Grid from '@material-ui/core';
+import CreateAirpayForm from '../airpays-page/create-airpay-form';
+
+const UpdateApiaryPage = () => {
+  return (
+    <Grid style={{ marginTop: '70px' }}>
+        <CreateAirpayForm />
+    </Grid>
+  )
+}
+
+export default UpdateApiaryPage;
