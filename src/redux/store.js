5785ac53d0180db61d7a6ccae701bda94fcb45fd
KostaLaz <kosta.lazarevski89@gmail.com>
2022-05-09 14:31:31 +0200

    gondolas reducer added

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from '../slices/authSlice/authSlice';
 import messageReducer from '../slices/messageSlice/messageSlice';
 import airpayReducer from '../slices/airpaySlice/airpaySlice';
+import singleApiaryReducer  from '../slices/singleApiarySlice/singleApiarySlice';
+import gondolasSlice from "../slices/gondolas-slice/gondolasSlice";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         message: messageReducer,
         airpays: airpayReducer,
+        apiary: singleApiaryReducer,
+        gondolas: gondolasSlice,
     },
 });
